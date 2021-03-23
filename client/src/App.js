8dd7eaa761ce56721fcb41ff6911ac718d84f846
JaeHongMin94 <moonshin123@naver.com>
2021-03-23 20:46:05 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -20,33 +20,6 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: 'https://placeimg.com/64/64/1',
-    name: '홍길동',
-    birthday: '961111',
-    gender: '남자',
-    job: '기획자',
-  },
-  {
-    id: 2,
-    image: 'https://placeimg.com/64/64/2',
-    name: '이순신',
-    birthday: '960315',
-    gender: '남자',
-    job: '프로그래머',
-  },
-  {
-    id: 3,
-    image: 'https://placeimg.com/64/64/3',
-    name: '고길동',
-    birthday: '960806',
-    gender: '남자',
-    job: '디자이너',
-  },
-];
-
 class App extends Component {
   state = {
     customers: '',
